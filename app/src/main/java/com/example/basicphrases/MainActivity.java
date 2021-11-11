9598d518ab3c29368a46d589352c4674aa4f2c7c
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-11 14:33:30 -0500

    Add playPhrase function

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.basicphrases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+        Button buttonPressed = (Button) view;
+        Log.i("Button Pressed", buttonPressed.getTag().toString());
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
