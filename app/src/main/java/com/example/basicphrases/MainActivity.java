d3ea91fb0c1dc11927a4d89157b501283c20037c
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-11 18:37:37 -0500

    Dropdown switch languages work

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -6,14 +6,20 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner dropdown;
+
     public void playPhrase(View view) {
+
         Button buttonPressed = (Button) view;
-        Log.i("Button Pressed", buttonPressed.getTag().toString());
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        String buttonTag = buttonPressed.getTag().toString() + dropdown.getSelectedItem().toString().toLowerCase();
+        Log.i("Button Pressed", buttonTag);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonTag, "raw", getPackageName()));
         mediaPlayer.start();
 
     }
@@ -22,5 +28,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"German", "Italian", "Spanish"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        dropdown.setAdapter(adapter);
+
     }
 }
