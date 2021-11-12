bc17ee5f762c726331d63e32fb4507ff068fce78
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-11 19:21:26 -0500

    Add Chinese

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner dropdown;
 
+
+
     public void playPhrase(View view) {
+
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
         Button buttonPressed = (Button) view;
         String buttonTag = buttonPressed.getTag().toString() + dropdown.getSelectedItem().toString().toLowerCase();
         Log.i("Button Pressed", buttonTag);
+
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
         MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonTag, "raw", getPackageName()));
         mediaPlayer.start();
 
@@ -30,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dropdown = findViewById(R.id.spinner1);
-        String[] items = new String[]{"German", "Italian", "Spanish"};
+        String[] items = new String[]{"Chinese", "German", "Italian", "Spanish"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         dropdown.setAdapter(adapter);
